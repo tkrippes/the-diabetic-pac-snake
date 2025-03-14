fc1c18b3d65a12a27afbc76a4e7ea3c05d40a916
Tom Krippes <tom.krippes@weinig.com>
2025-03-14 11:10:59 +0100

    feat: add direction to snake
    fix positioning of snake parts (character2d instead of sprite2d position)
    next: control the movement of the snake

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -1,26 +1,49 @@
 extends Node2D
 
+enum Direction {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+}
+
 @export var tile_size: int = 32
 
 @onready var head: CharacterBody2D = $Head
 @onready var bodies: Array[CharacterBody2D] = [$Body]
 @onready var tail: CharacterBody2D = $Tail
 
+var _current_direction := Direction.RIGHT;
+
 
 func _on_movement_timer_timeout() -> void:
-	_move_head()
-	_move_bodies()
 	_move_tail()
+	_move_bodies()
+	_move_head()
 
 	
 func _move_head() -> void:
-	head.position += Vector2(tile_size, 0)
+	var position_offset: Vector2
+	
+	match _current_direction:
+		Direction.UP:
+			position_offset = Vector2(0, -tile_size)
+		Direction.DOWN:
+			position_offset = Vector2(0, tile_size)
+		Direction.LEFT:
+			position_offset = Vector2(-tile_size, 0)
+		Direction.RIGHT:
+			position_offset = Vector2(tile_size, 0)
+	
+	head.position += position_offset
 	
 	
 func _move_bodies() -> void:
-	for body in bodies:
-		body.position += Vector2(tile_size, 0)
+	bodies.front().position = head.position
+	
+	for i in range(1, bodies.size()):
+		bodies[i].position = bodies[i - 1].position
 		
 		
 func _move_tail() -> void:
-	tail.position += Vector2(tile_size, 0)
+	tail.position = bodies.back().position
