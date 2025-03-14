7d85b2c56ce778510e0df6f59528324940c974cf
Tom Krippes <tom.krippes@weinig.com>
2025-03-14 13:06:27 +0100

    feat: add head rotation of snake
    fix position of snake to align with grid
    fix issue with reversing snake when changing direction quickl
    refactor functions

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -10,33 +10,53 @@ enum Direction {
 @export var tile_size: int = 32
 
 @onready var head: CharacterBody2D = $Head
+@onready var head_sprite: Sprite2D = $Head/Sprite
 @onready var bodies: Array[CharacterBody2D] = [$Body]
+@onready var bodySprites: Array[Sprite2D] = [$Body/Sprite]
 @onready var tail: CharacterBody2D = $Tail
+@onready var tailSprite: Sprite2D = $Tail/Sprite
 
-var _current_direction := Direction.RIGHT;
+
+var _last_direction := Direction.RIGHT
+var _current_direction := Direction.RIGHT
 
 
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
-		if _current_direction != Direction.DOWN:
+		if _last_direction != Direction.DOWN:
 			_current_direction = Direction.UP
 	elif Input.is_action_pressed("move_down"):
-		if _current_direction != Direction.UP:
+		if _last_direction != Direction.UP:
 			_current_direction = Direction.DOWN
 	elif Input.is_action_pressed("move_left"):
-		if _current_direction != Direction.RIGHT:
+		if _last_direction != Direction.RIGHT:
 			_current_direction = Direction.LEFT
 	elif Input.is_action_pressed("move_right"):
-		if _current_direction != Direction.LEFT:
+		if _last_direction != Direction.LEFT:
 			_current_direction = Direction.RIGHT
 
 
 func _on_movement_timer_timeout() -> void:
-	_move_tail()
-	_move_bodies()
-	_move_head()
+	_update_tail()
+	_update_bodies()
+	_update_head()
 
+
+func _update_tail() -> void:
+	tail.position = bodies.back().position
+
+func _update_bodies() -> void:
+	bodies.front().position = head.position
 	
+	for i in range(1, bodies.size()):
+		bodies[i].position = bodies[i - 1].position
+
+
+func _update_head() -> void:
+	_move_head()
+	_rotate_head()
+
+
 func _move_head() -> void:
 	var position_offset: Vector2
 	
@@ -51,14 +71,20 @@ func _move_head() -> void:
 			position_offset = Vector2(tile_size, 0)
 	
 	head.position += position_offset
-	
-	
-func _move_bodies() -> void:
-	bodies.front().position = head.position
-	
-	for i in range(1, bodies.size()):
-		bodies[i].position = bodies[i - 1].position
-		
-		
-func _move_tail() -> void:
-	tail.position = bodies.back().position
+	_last_direction = _current_direction
+
+
+func _rotate_head() -> void:
+	match _current_direction:
+		Direction.UP:
+			head_sprite.flip_h = false
+			head_sprite.rotation_degrees = -90
+		Direction.DOWN:
+			head_sprite.flip_h = false
+			head_sprite.rotation_degrees = 90
+		Direction.LEFT:
+			head_sprite.flip_h = true
+			head_sprite.rotation_degrees = 0
+		Direction.RIGHT:
+			head_sprite.flip_h = false
+			head_sprite.rotation_degrees = 0
