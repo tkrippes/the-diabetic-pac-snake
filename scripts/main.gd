cbef93d513532f292deb5e8a81c958b962146178
Tom Krippes <tom.krippes@weinig.com>
2025-03-26 14:28:46 +0100

    audio: add background music
    refactor some functions in main

--- scripts/main.gd
+++ scripts/main.gd
@@ -23,15 +23,9 @@ func _process(_delta: float) -> void:
 			_restart_game()
 	elif Input.is_action_just_pressed("pause"):
 		if _state == GameState.RUNNING:
-			get_tree().paused = true
-			user_interface.show_pause_label()
-			sound_controller.play_pause_sound()
-			_state = GameState.PAUSED
+			_pause_game()
 		elif _state == GameState.PAUSED:
-			get_tree().paused = false
-			user_interface.hide_pause_label()
-			sound_controller.play_unpause_sound()
-			_state = GameState.RUNNING
+			_unpause_game()
 
 	
 			
@@ -40,23 +34,41 @@ func _on_snake_ate_fruit() -> void:
 	
 			
 func _on_snake_died() -> void:
-	_state = GameState.GAME_OVER
 	user_interface.show_game_over_label()
-			
-			
+	sound_controller.stop_background_music()
+	_state = GameState.GAME_OVER
+
+
 func _start_game() -> void:
 	_set_score(0)
-	_state = GameState.RUNNING
 	user_interface.show_score_label()
+	sound_controller.start_background_music()
 	world.start()
-	
+	_state = GameState.RUNNING
+
 
 func _restart_game() -> void:
-	_state = GameState.START
 	user_interface.show_start_label()
 	world.reset()
-	
-	
+	_state = GameState.START
+
+
+func _pause_game() -> void:
+	get_tree().paused = true
+	user_interface.show_pause_label()
+	sound_controller.stop_background_music()
+	sound_controller.play_pause_sound()
+	_state = GameState.PAUSED
+
+
+func _unpause_game() -> void:
+	get_tree().paused = false
+	user_interface.hide_pause_label()
+	sound_controller.play_background_music()
+	sound_controller.play_unpause_sound()
+	_state = GameState.RUNNING
+
+
 func _set_score(score: int) -> void:
 	_score = score
 	user_interface.update_score(_score)
--- scripts/sound_controller.gd
+++ scripts/sound_controller.gd
@@ -1,8 +1,25 @@
 class_name SoundController
 extends Node
 
+@onready var background_music: AudioStreamPlayer = $BackgroundMusic
 @onready var pause_sound: AudioStreamPlayer = $PauseSound
 @onready var unpause_sound: AudioStreamPlayer = $UnpauseSound
+
+var _background_music_position: float = 0.0
+
+
+func start_background_music() -> void:
+    _background_music_position = 0.0
+    play_background_music()
+
+
+func play_background_music() -> void:
+    background_music.play(_background_music_position)
+
+
+func stop_background_music() -> void:
+    _background_music_position = background_music.get_playback_position()
+    background_music.stop()
 
 
 func play_pause_sound() -> void:
