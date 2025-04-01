ccaa46b8a8de2f43edaebf06aef9e42e17135e22
Tom Krippes <tom.krippes@weinig.com>
2025-04-01 11:02:19 +0200

    refactor: rename scene to item scene for spawn settings / controller
    rename functions correspondingly

--- scripts/world/spawn_controller.gd
+++ scripts/world/spawn_controller.gd
@@ -4,7 +4,7 @@ extends Node
 @export var spawn_settings: SpawnSettings
 @export var arena_settings: ArenaSettings
 
-var _current_node: Node2D = null
+var _current_item: Node2D = null
 var _start_timer := false
 var _elapsed_time := 0.0
 
@@ -13,11 +13,11 @@ func _process(delta: float) -> void:
 	if _start_timer:
 		_elapsed_time += delta
 
-		if _current_node == null and _elapsed_time > spawn_settings.spawn_timeout:
-			_spawn_node(arena_settings.get_random_position())
+		if _current_item == null and _elapsed_time > spawn_settings.spawn_timeout:
+			_spawn_item(arena_settings.get_random_position())
 			_elapsed_time = 0.0
-		elif _current_node != null and _elapsed_time > spawn_settings.despawn_timeout:
-			_despawn_node()
+		elif _current_item != null and _elapsed_time > spawn_settings.despawn_timeout:
+			_despawn_item()
 			_elapsed_time = 0.0
 
 
@@ -28,8 +28,8 @@ func start() -> void:
 func stop() -> void:
 	_start_timer = false
 	_elapsed_time = 0.0
-	if _current_node != null:
-		_despawn_node()
+	if _current_item != null:
+		_despawn_item()
 
 
 func reset_timer() -> void:
@@ -37,20 +37,20 @@ func reset_timer() -> void:
 	
 	
 func get_occupied_positions() -> Array[Vector2]:
-	if _current_node == null:
+	if _current_item == null:
 		return []
 	
-	return [_current_node.global_position]
+	return [_current_item.global_position]
 
 
-func _spawn_node(position: Vector2i) -> void:
-	var scene: PackedScene = spawn_settings.scenes.pick_random()
-	_current_node = scene.instantiate()
-	_current_node.global_position = position
+func _spawn_item(position: Vector2i) -> void:
+	var item_scene: PackedScene = spawn_settings.item_scenes.pick_random()
+	_current_item = item_scene.instantiate()
+	_current_item.global_position = position
 	
-	add_child(_current_node)
+	add_child(_current_item)
 
 
-func _despawn_node() -> void:
-	_current_node.queue_free()
-	_current_node = null
+func _despawn_item() -> void:
+	_current_item.queue_free()
+	_current_item = null
--- scripts/world/spawn_settings.gd
+++ scripts/world/spawn_settings.gd
@@ -1,6 +1,6 @@
 class_name SpawnSettings
 extends Resource
 
-@export var scenes: Array[PackedScene]
+@export var item_scenes: Array[PackedScene]
 @export var spawn_timeout: float
 @export var despawn_timeout: float
